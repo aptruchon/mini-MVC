7400042746f21210686b6d5bf99dad131154abd1
aptruchon <aptruchon@gmail.com>
2022-06-30 15:20:16 -0400

    mini-MVC vierge

---
+++ controleurs/NouveauControleur.cls.php
@@ -0,0 +1,12 @@
+<?php
+    class Controleur extends Controleur
+    {
+        /**
+         * Méthode invoquée par défault si aucune action n'est indiqué
+         */
+        public function index($params)
+        {
+            //$this->gabarit->affecterActionParDefaut("tout");
+            //$this->tout($params);
+        }
+    }
